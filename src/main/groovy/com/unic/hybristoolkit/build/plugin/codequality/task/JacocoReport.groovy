428f652fb604615f9d01be3c1428214dca72756b
Lennart Jütte <lennart.juette@unic.com>
2022-01-21 15:27:55 +0100

    Add jacocoReport task and decouple from hybrisantwrapper

---
+++ src/main/groovy/com/unic/hybristoolkit/build/plugin/codequality/task/JacocoReport.groovy
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2022 Unic AG
+ */
+
+package com.unic.hybristoolkit.build.plugin.codequality.task
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.file.FileCollection
+import org.gradle.api.provider.Property
+import org.gradle.api.tasks.TaskAction
+import groovy.io.FileType
+
+/**
+ * Wraps a javaexec to execute jacoco-cli to generate an XML report from exec files.
+ */
+class JacocoReport extends DefaultTask {
+
+    final Property<FileCollection> jacocoCliClasspath = project.objects.property(FileCollection)
+    def jacocoCliWorkingDir
+    File hybrisDir = new File("${project.projectDir}/hybris")
+
+    @TaskAction
+    void sonarrunner() {
+        File customCodeDir = new File(hybrisDir, "/bin/custom")
+        def classDirs = []
+        customCodeDir.eachFileRecurse (FileType.DIRECTORIES) { dir ->
+            if (dir.name == 'classes') {
+                classDirs << "--classfiles=${dir}"
+            }
+        }
+
+        File jacocoLogDir = new File(hybrisDir, "/log/jacoco")
+        def execFiles = []
+        jacocoLogDir.eachFileRecurse (FileType.FILES) { file ->
+            if (file.name.endsWith('.exec')) {
+                execFiles << file
+            }
+        }
+        project.javaexec {
+            classpath = jacocoCliClasspath.get()
+            main = 'org.jacoco.cli.internal.Main'
+            workingDir = jacocoCliWorkingDir
+            systemProperty 'java.awt.headles', 'true'
+            args 'report'
+            args execFiles
+            args "--xml=${jacocoLogDir}/jacoco-report.xml"
+            args classDirs
+        }
+    }
+}
